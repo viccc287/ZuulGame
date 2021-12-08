a6d7325962238e24e3796b658b94bbb141848026
David Paredes <a14003834@alumnos.uady.mx>
2021-12-08 12:06:19 -0600

    clase Parser añadida

---
+++ Parser.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Parser {
+    private Scanner reader;
+    private CommandWords commands;
+    public Parser()
+    {
+        reader = new Scanner(System.in);
+    }
+    public Commands getCommand()
+    {
+        String inputLine;
+        String word1 = null;
+        String word2 = null;
+
+        System.out.print("> ");
+
+        inputLine = reader.nextLine();
+
+        Scanner tokenizer = new Scanner(inputLine);
+        if(tokenizer.hasNext()) {
+            word1 = tokenizer.next();
+            if(tokenizer.hasNext()) {
+                word2 = tokenizer.next();
+            }
+        }
+
+        if(commands.isCommand(word1)) {
+            return new Commands(word1, word2);
+        }
+        else {
+            return new Commands(null, word2);
+        }
+    }
+}
+
+
+
