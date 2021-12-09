fd4266d124a29262f1372939512969706e642268
David Paredes <a14003834@alumnos.uady.mx>
2021-12-09 11:32:28 -0600

    Refactor Extract Method, se creo la funcion parsingMethod

--- CommandParser.java
+++ CommandParser.java
@@ -7,25 +7,26 @@ public class CommandParser {
         commands = CommandWords.getInstance();
         reader = new Scanner(System.in);
     }
-    public Command getCommand()
-    {
-        String inputLine;
+    public Command getCommand() {
         String word_action = null;
         String word_direction = null;
         System.out.print(" > ");
+        parsingMethod();
+        if(commands.isCommand(word_action)) {
+            return new Command(word_action, word_direction);
+        } else {
+            return new Command(null, word_direction);
+        }
+    }
+    private void parsingMethod(){
+        String inputLine;
         inputLine = reader.nextLine();
         Scanner inputData = new Scanner(inputLine);
         while(inputData.hasNext()){
-            word_action = inputData.next();
+            String word_action = inputData.next();
             if(inputData.hasNext()) {
-                word_direction = inputData.next();
+                String word_direction = inputData.next();
             }
-        }
-        if(commands.isCommand(word_action)) {
-            return new Command(word_action, word_direction);
-        }
-        else {
-            return new Command(null, word_direction);
         }
     }
 
