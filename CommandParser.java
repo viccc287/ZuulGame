bc999e29b267d36a8f56c3ee8335ebf34dfc22c1
viccc287 <vicnav28@gmail.com>
2021-12-09 14:09:18 -0600

    Bug fix em CommandParser

--- CommandParser.java
+++ CommandParser.java
@@ -10,23 +10,24 @@ public class CommandParser {
     public Command getCommand() {
         String word_action = null;
         String word_direction = null;
+
         System.out.print(" > ");
-        parsingMethod(word_action,word_direction);
-        if(commands.isCommand(word_action)) {
-            return new Command(word_action, word_direction);
-        } else {
-            return new Command(null, word_direction);
-        }
-    }
-    private void parsingMethod(String word_action, String word_direction){
         String inputLine;
+
         inputLine = reader.nextLine();
         Scanner inputData = new Scanner(inputLine);
+
         while(inputData.hasNext()){
             word_action = inputData.next();
             if(inputData.hasNext()) {
                 word_direction = inputData.next();
             }
+        }
+
+        if(commands.isCommand(word_action)) {
+            return new Command(word_action, word_direction);
+        } else {
+            return new Command(null, word_direction);
         }
     }
 
