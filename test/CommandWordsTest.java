05fa8d5ad3d927347233078f5f5196a4e2697493
viccc287 <vicnav28@gmail.com>
2021-12-09 19:45:54 -0600

    Prueba unitaria isCommand() de CommandWords

---
+++ test/CommandWordsTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import main.CommandWords;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(value = Parameterized.class)
+public class CommandWordsTest {
+
+    CommandWords commandWords;
+    String[] testCommands;
+
+    @Parameterized.Parameters
+    public static Collection<Object> getData(){
+        return  Arrays.asList(new Object[][]{
+                {new String[]{"go","quit","help","go"}},//Ejemplo valido
+                {new String[]{"go","quit","hep","run"}},//Ejemplo fallo
+        });
+    }
+
+    public CommandWordsTest(String[] testCommands){
+        this.testCommands = testCommands;
+    }
+
+    @Before
+    public void before(){
+        commandWords = CommandWords.getInstance();
+    }
+    @Test
+    public void isCommandTest() {
+        List<String> expected = new ArrayList<>();
+        List<String> invalidCommands = new ArrayList<>();
+        for (String commandString : testCommands) {
+            if (!commandWords.isCommand(commandString)) {
+                invalidCommands.add(commandString);
+            }
+        }
+        assertEquals(expected,invalidCommands);
+    }
+}
